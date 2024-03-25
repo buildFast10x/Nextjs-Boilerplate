f2ed54c1554899801a8b8109966ed10fc1f53921
Uday032 <rahul.uday07@gmail.com>
2024-03-25 19:23:09 +0530

    eslint properties disable

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true,
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
     },
 }
 
