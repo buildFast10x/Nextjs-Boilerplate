15c716e27bb3eef571defdf8434b8e7e1a348622
Uday032 <rahul.uday07@gmail.com>
2024-03-25 19:25:19 +0530

    bug fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
     experimental: {
         missingSuspenseWithCSRBailout: false,
     },
